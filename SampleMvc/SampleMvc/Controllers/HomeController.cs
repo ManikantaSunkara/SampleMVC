ab6ef95770b893f837394ab1324e1dcfea6a6ba1
ManikantaSunkara <sunkaramanikanta@yahoo.com>
2021-02-03 17:56:35 +0530

    code

---
+++ SampleMvc/SampleMvc/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SampleMvc.Models;
+
+namespace SampleMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            List<IndexModel> newList = new List<IndexModel>();
+
+            IndexModel newindexModel = new IndexModel
+            {
+                ID = 1,
+                Description = "firstItem",
+                Comments = "This is the first instance of the model"
+            };
+
+
+            IndexModel newindexModel2 = new IndexModel
+            {
+                ID = 2,
+                Description = "secondItem",
+                Comments = "This is the second instance of the model"
+            };
+            newList.Add(newindexModel);
+            newList.Add(newindexModel2);
+            return View(newList);
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+
+        }
+
+
+    }
+}
---
+++ SampleMvc/SampleMvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SampleMvc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleMvc")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2f3dd1b4-19fc-4514-b25d-d1bea0b9050e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
